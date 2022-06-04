f2556e40c0e8929de3d6b4b4d72776de4448813d
timfvb <timvbo@gmx.de>
2022-06-05 01:46:13 +0200

    Initial commit

---
+++ NetRoad/NRoadClient.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * ===============================================
+ * N-ROAD OPEN SOURCE NETWORK LIBRARY PROJECT
+ * BY https://github.com/timfvb
+ * ===============================================
+ */
+
+using System.Net;
+using System.Text;
+using NetRoad.Network;
+
+namespace NetRoad;
+
+public class NRoadClient
+{
+    private Client _client;
+
+    /// <summary>
+    /// Initialize a new NRoad Client Object
+    /// </summary>
+    /// <param name="endPoint">Pair from IP and port</param>
+    /// <param name="encoding">Encoding type</param>
+    public NRoadClient(EndPoint endPoint, Encoding encoding)
+    {
+        _client = new Client(endPoint, encoding);
+    }
+
+    /// <summary>
+    /// Initialize a new NRoad Client Object
+    /// </summary>
+    /// <param name="address">Address from destination</param>
+    /// <param name="port">Port from destination</param>
+    /// <param name="encoding"></param>
+    /// <exception cref="FormatException">Invalid IPv4 Format</exception>
+    /// <exception cref="ArgumentException">Invalid Port Range</exception>
+    public NRoadClient(string address, int port, Encoding encoding)
+    {
+        // Check validation of ipv4 address format
+        if (!IPAddress.TryParse(address, out var ipAddress))
+            throw new FormatException("Address must be a valid IPv4 address format.");
+        
+        // Check validation of the port format
+        if (port > Math.Pow(2, 16) - 1)
+            throw new ArgumentException("Port range format is wrong. Port Range: 0-65535.");
+        
+        // Check encoding parameter 
+        if (encoding == null)
+            throw new ArgumentNullException(nameof(encoding));
+        
+        // Pair ipv4 and port to a endpoint
+        var endpoint = new IPEndPoint(ipAddress, port);
+        
+        // Create new client object
+        _client = new Client(endpoint, encoding);
+    }
+    
+    public bool Connect()
+    {
+        return _client.Connect();
+    }
+}
---
+++ NetRoad/Network/Client.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * ===============================================
+ * N-ROAD OPEN SOURCE NETWORK LIBRARY PROJECT
+ * BY https://github.com/timfvb
+ * ===============================================
+ */
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetRoad.Network;
+
+public class Client
+{
+    private Socket _socket;
+    private EndPoint _endPoint;
+    private Encoding _encoding;
+    
+    public Client(EndPoint endPoint, Encoding encoding)
+    {
+        _socket = new Socket(GetCurrentAddressFamily(), SocketType.Stream, ProtocolType.Tcp);
+        _endPoint = endPoint;
+        _encoding = encoding;
+    }
+
+    public bool Connect()
+    {
+        try
+        {
+            _socket.Connect(_endPoint);
+            return true;
+        }
+        catch (Exception e)
+        {
+            // Destination not reachable
+            if (e is SocketException)
+                return false;
+            throw;
+        }
+    }
+
+    public void Send(string content, int timeout = 3000)
+    {
+        // Check if client is connected
+        if (!_socket.Connected)
+            throw new Exception("NRoadClient is not connected");
+
+        // Set send timeout
+        _socket.SendTimeout = timeout; 
+            
+        // Encode string to byte array
+        var bytes = _encoding.GetBytes(content);
+        
+        // Send bytes to the destination
+        _socket.Send(bytes);
+    }
+
+    private static AddressFamily GetCurrentAddressFamily()
+    {
+        var hostInfo = Dns.GetHostEntry(Dns.GetHostName());  
+        var ipAddress = hostInfo.AddressList[0];
+        
+        return ipAddress.AddressFamily;
+    }
+}
